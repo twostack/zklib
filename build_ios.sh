3dd2ade1f13c12fe0b19f0f5c01b19580c05b238
Stephan February <stephan@werkswinkel.com>
2024-01-28 12:36:29 +0800

    added ios library codesigning

--- build_ios.sh
+++ build_ios.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
-# export GOOS=ios
-# export GOARCH=arm64
-# export SDK=iphoneos
-# export CGO_CFLAGS="-fembed-bitcode"
-# 
-# export SDK_PATH=`xcrun --sdk $SDK --show-sdk-path`
-# export CLANG=`xcrun --sdk $SDK --find clang`
-# export CARCH="arm64"  # if compiling for iPhone
-
-#go build -o lib/ios/zklib.a
-
 go get -d golang.org/x/mobile/cmd/gomobile
 
 gomobile bind -target=ios/arm64 -ldflags="-s -w" -o build/ios/Zklib.xcframework
+
+codesign --sign "32XLPKQ5TF" build/ios/Zklib.xcframework
