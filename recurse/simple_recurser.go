80516bfb0c89210ee6fc1e62fa1903fca7952e0e
Stephan February <stephan.february@token.im>
2024-03-06 12:24:38 +0800

    refactored recursive verifier

---
+++ recurse/simple_recurser.go
@@ -0,0 +1,112 @@
+package recurse
+
+import (
+	"crypto/sha256"
+	"github.com/consensys/gnark-crypto/ecc"
+	native_plonk "github.com/consensys/gnark/backend/plonk"
+	"github.com/consensys/gnark/backend/witness"
+	"github.com/consensys/gnark/constraint"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/scs"
+	"github.com/consensys/gnark/std/algebra/native/sw_bls12377"
+	"github.com/consensys/gnark/std/math/uints"
+	"github.com/consensys/gnark/std/recursion/plonk"
+	"github.com/consensys/gnark/test/unsafekzg"
+	"math/big"
+)
+
+func setupBaseCase(innerField *big.Int) (constraint.ConstraintSystem, native_plonk.ProvingKey, native_plonk.VerifyingKey, error) {
+
+	baseCcs, err := frontend.Compile(innerField, scs.NewBuilder,
+		&Sha256CircuitBaseCase[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine, sw_bls12377.GT]{})
+
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	srs, srsLagrange, err := unsafekzg.NewSRS(baseCcs)
+
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	innerPK, innerVK, err := native_plonk.Setup(baseCcs, srs, srsLagrange)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	return baseCcs, innerPK, innerVK, nil
+}
+
+func createBaseCaseProof(fullTxBytes []byte, prefixBytes []byte, prevTxnIdBytes []byte, postfixBytes []byte) (
+	constraint.ConstraintSystem,
+	native_plonk.VerifyingKey,
+	native_plonk.ProvingKey,
+	witness.Witness,
+	native_plonk.Proof,
+) {
+
+	innerField := ecc.BLS12_377.ScalarField()
+	outerField := ecc.BW6_761.ScalarField()
+
+	innerCcs, provingKey, verifyingKey, err := setupBaseCase(innerField)
+	if err != nil {
+		panic(err)
+	}
+
+	firstHash := sha256.Sum256(fullTxBytes)
+	genesisTxId := sha256.Sum256(firstHash[:])
+
+	genesisWitness, err := createBaseCaseWitness(prefixBytes, postfixBytes, prevTxnIdBytes, genesisTxId)
+
+	proof, err := native_plonk.Prove(innerCcs, provingKey, genesisWitness, plonk.GetNativeProverOptions(outerField, innerField))
+
+	return innerCcs, verifyingKey, provingKey, genesisWitness, proof
+}
+
+func createBaseCaseWitness(
+	prefixBytes []byte,
+	postFixBytes []byte,
+	prevTxnIdBytes []byte,
+	currTxId [32]byte,
+) (witness.Witness, error) {
+
+	innerAssignment := Sha256CircuitBaseCase[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine, sw_bls12377.GT]{}
+
+	//assign the current Txn data
+	copy(innerAssignment.CurrTxPrefix[:], uints.NewU8Array(prefixBytes))
+	copy(innerAssignment.CurrTxPost[:], uints.NewU8Array(postFixBytes))
+	copy(innerAssignment.PrevTxId[:], uints.NewU8Array(prevTxnIdBytes))
+	copy(innerAssignment.CurrTxId[:], uints.NewU8Array(currTxId[:]))
+	copy(innerAssignment.TokenId[:], uints.NewU8Array(currTxId[:])) //base case tokenId == txId
+
+	innerWitness, err := frontend.NewWitness(&innerAssignment, ecc.BLS12_377.ScalarField())
+	if err != nil {
+		return nil, err
+	}
+	return innerWitness, nil
+}
+
+func createOuterAssignment(
+	circuitWitness plonk.Witness[sw_bls12377.ScalarField],
+	circuitProof plonk.Proof[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine],
+	prefixBytes []byte, prevTxnIdBytes []byte, postFixBytes []byte, fullTxBytes []byte) Sha256Circuit[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine, sw_bls12377.GT] {
+
+	outerAssignment := Sha256Circuit[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine, sw_bls12377.GT]{
+		PreviousWitness: circuitWitness,
+		PreviousProof:   circuitProof,
+	}
+
+	firstHash := sha256.Sum256(fullTxBytes)
+	currTxId := sha256.Sum256(firstHash[:])
+
+	copy(outerAssignment.CurrTxPrefix[:], uints.NewU8Array(prefixBytes))
+	copy(outerAssignment.CurrTxPost[:], uints.NewU8Array(postFixBytes))
+	copy(outerAssignment.PrevTxId[:], uints.NewU8Array(prevTxnIdBytes))
+	copy(outerAssignment.CurrTxId[:], uints.NewU8Array(currTxId[:]))
+
+	tokenId := [32]byte{}
+	copy(tokenId[:], prevTxnIdBytes)
+	copy(outerAssignment.TokenId[:], uints.NewU8Array(tokenId[:]))
+
+	return outerAssignment
+}
--- recurse/txv2recurse_test.go
+++ recurse/txv2recurse_test.go
@@ -59,48 +59,49 @@ func TestBaseCase(t *testing.T) {
 }
 
 // circuitVk plonk.VerifyingKey[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine],
-func createBaseCaseProof() (
-	constraint.ConstraintSystem,
-	native_plonk.VerifyingKey,
-	native_plonk.ProvingKey,
-	witness.Witness,
-	native_plonk.Proof,
-) {
-
-	innerField := ecc.BLS12_377.ScalarField()
-	//outerField := ecc.BLS12_377.ScalarField()
-	outerField := ecc.BW6_761.ScalarField()
-
-	innerCcs, provingKey, verifyingKey, err := setupBaseCase(innerField)
-	if err != nil {
-		panic(err)
-	}
-
-	fullTxBytes, _ := hex.DecodeString("020000000190bc0a14e94cdd565265d79c4f9bed0f6404241f3fb69d6458b30b41611317f7000000004847304402204e643ff6ed0e3c3e1e83f3e2c74a9d0613849bb624c1d12351f1152cf91ebc1f02205deaa38e3f8f8e43d1979f999c03ffa65b9087c1a6545ecffa2b7898c042bcb241feffffff0200ca9a3b000000001976a914662db6c1a68cdf035bfb9c6580550eb3520caa9d88ac40276bee000000001976a9142dbbeab87bd7a8fca8b2761e5d798dfd76d5af4988ac6f000000")
-	prefixBytes, _ := hex.DecodeString("0200000001")
-	prevTxnIdBytes, _ := hex.DecodeString("90bc0a14e94cdd565265d79c4f9bed0f6404241f3fb69d6458b30b41611317f7")
-	postFixBytes, _ := hex.DecodeString("000000004847304402204e643ff6ed0e3c3e1e83f3e2c74a9d0613849bb624c1d12351f1152cf91ebc1f02205deaa38e3f8f8e43d1979f999c03ffa65b9087c1a6545ecffa2b7898c042bcb241feffffff0200ca9a3b000000001976a914662db6c1a68cdf035bfb9c6580550eb3520caa9d88ac40276bee000000001976a9142dbbeab87bd7a8fca8b2761e5d798dfd76d5af4988ac6f000000")
-
-	firstHash := sha256.Sum256(fullTxBytes)
-	genesisTxId := sha256.Sum256(firstHash[:])
-
-	//fmt.Println(hex.EncodeToString(genesisTxId[:]))
-	// create full genesis witness (placeholders, prevTxnIdBytes is empty
-	//vk, err := plonk.ValueOfVerifyingKey[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine](verifyingKey)
-	genesisWitness, err := createBaseCaseWitness(prefixBytes, postFixBytes, prevTxnIdBytes, genesisTxId)
-
-	proof, err := native_plonk.Prove(innerCcs, provingKey, genesisWitness, plonk.GetNativeProverOptions(outerField, innerField))
-
-	return innerCcs, verifyingKey, provingKey, genesisWitness, proof
-}
+//func createBaseCaseProof() (
+//	constraint.ConstraintSystem,
+//	native_plonk.VerifyingKey,
+//	native_plonk.ProvingKey,
+//	witness.Witness,
+//	native_plonk.Proof,
+//) {
+//
+//	innerField := ecc.BLS12_377.ScalarField()
+//	//outerField := ecc.BLS12_377.ScalarField()
+//	outerField := ecc.BW6_761.ScalarField()
+//
+//	innerCcs, provingKey, verifyingKey, err := setupBaseCase(innerField)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	fullTxBytes, _ := hex.DecodeString("020000000190bc0a14e94cdd565265d79c4f9bed0f6404241f3fb69d6458b30b41611317f7000000004847304402204e643ff6ed0e3c3e1e83f3e2c74a9d0613849bb624c1d12351f1152cf91ebc1f02205deaa38e3f8f8e43d1979f999c03ffa65b9087c1a6545ecffa2b7898c042bcb241feffffff0200ca9a3b000000001976a914662db6c1a68cdf035bfb9c6580550eb3520caa9d88ac40276bee000000001976a9142dbbeab87bd7a8fca8b2761e5d798dfd76d5af4988ac6f000000")
+//	prefixBytes, _ := hex.DecodeString("0200000001")
+//	prevTxnIdBytes, _ := hex.DecodeString("90bc0a14e94cdd565265d79c4f9bed0f6404241f3fb69d6458b30b41611317f7")
+//	postFixBytes, _ := hex.DecodeString("000000004847304402204e643ff6ed0e3c3e1e83f3e2c74a9d0613849bb624c1d12351f1152cf91ebc1f02205deaa38e3f8f8e43d1979f999c03ffa65b9087c1a6545ecffa2b7898c042bcb241feffffff0200ca9a3b000000001976a914662db6c1a68cdf035bfb9c6580550eb3520caa9d88ac40276bee000000001976a9142dbbeab87bd7a8fca8b2761e5d798dfd76d5af4988ac6f000000")
+//
+//
+//	firstHash := sha256.Sum256(fullTxBytes)
+//	genesisTxId := sha256.Sum256(firstHash[:])
+//
+//	//fmt.Println(hex.EncodeToString(genesisTxId[:]))
+//	// create full genesis witness (placeholders, prevTxnIdBytes is empty
+//	//vk, err := plonk.ValueOfVerifyingKey[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine](verifyingKey)
+//	genesisWitness, err := createBaseCaseWitness(prefixBytes, postFixBytes, prevTxnIdBytes, genesisTxId)
+//
+//	proof, err := native_plonk.Prove(innerCcs, provingKey, genesisWitness, plonk.GetNativeProverOptions(outerField, innerField))
+//
+//	return innerCcs, verifyingKey, provingKey, genesisWitness, proof
+//}
 
 func TestInitialRecursion(t *testing.T) {
 
 	assert := test.NewAssert(t)
 
 	innerField := ecc.BLS12_377.ScalarField()
-	outerField := ecc.BLS12_377.ScalarField()
-	//outerField := ecc.BW6_761.ScalarField()
+	//outerField := ecc.BLS12_377.ScalarField()
+	outerField := ecc.BW6_761.ScalarField()
 
 	prefixBytes, _ := hex.DecodeString("0200000001")
 	prevTxnIdBytes, _ := hex.DecodeString("faf3013aab53ae122e6cfdef7720c7a785fed4ce7f8f3dd19379f31e62651c71")
@@ -120,7 +121,7 @@ func TestInitialRecursion(t *testing.T) {
 	copy(innerAssignment.TokenId[:], uints.NewU8Array(prevTxnIdBytes[:]))  //base case tokenId == txId
 	previousWitness, err := frontend.NewWitness(&innerAssignment, ecc.BLS12_377.ScalarField())
 
-	innerCcs, verifyingKey, provingKey, genesisWitness, previousProof := createBaseCaseProof()
+	innerCcs, verifyingKey, provingKey, genesisWitness, previousProof := createBaseCaseProof(fullTxBytes, prefixBytes, prevTxnIdBytes, postFixBytes)
 
 	//outerField := ecc.BW6_761.ScalarField()
 	innerWitness, err := plonk.ValueOfWitness[sw_bls12377.ScalarField](previousWitness) //FIXME: Check if this breaks because witness structure sizes
@@ -147,29 +148,6 @@ func TestInitialRecursion(t *testing.T) {
 	//gw, err := plonk.ValueOfWitness[sw_bls12377.ScalarField](genesisWitness)
 	//issuanceWitness, err := createFullWitness(gw, previousProof, vk, prefixBytes, postFixBytes, genesisPrevTxnIdBytes, genesisTxId, innerField)
 	//issuanceProof, err := native_plonk.Prove(innerCcs, provingKey, genesisWitness, plonk.GetNativeProverOptions(outerField, innerField))
-}
-
-func createBaseCaseWitness(
-	prefixBytes []byte,
-	postFixBytes []byte,
-	prevTxnIdBytes []byte,
-	currTxId [32]byte,
-) (witness.Witness, error) {
-
-	innerAssignment := Sha256CircuitBaseCase[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine, sw_bls12377.GT]{}
-
-	//assign the current Txn data
-	copy(innerAssignment.CurrTxPrefix[:], uints.NewU8Array(prefixBytes))
-	copy(innerAssignment.CurrTxPost[:], uints.NewU8Array(postFixBytes))
-	copy(innerAssignment.PrevTxId[:], uints.NewU8Array(prevTxnIdBytes))
-	copy(innerAssignment.CurrTxId[:], uints.NewU8Array(currTxId[:]))
-	copy(innerAssignment.TokenId[:], uints.NewU8Array(currTxId[:])) //base case tokenId == txId
-
-	innerWitness, err := frontend.NewWitness(&innerAssignment, ecc.BLS12_377.ScalarField())
-	if err != nil {
-		return nil, err
-	}
-	return innerWitness, nil
 }
 
 func createFullWitness(
@@ -205,28 +183,6 @@ func createFullWitness(
 	return innerWitness, nil
 }
 
-func setupBaseCase(innerField *big.Int) (constraint.ConstraintSystem, native_plonk.ProvingKey, native_plonk.VerifyingKey, error) {
-
-	baseCcs, err := frontend.Compile(innerField, scs.NewBuilder,
-		&Sha256CircuitBaseCase[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine, sw_bls12377.GT]{})
-
-	if err != nil {
-		return nil, nil, nil, err
-	}
-
-	srs, srsLagrange, err := unsafekzg.NewSRS(baseCcs)
-
-	if err != nil {
-		return nil, nil, nil, err
-	}
-
-	innerPK, innerVK, err := native_plonk.Setup(baseCcs, srs, srsLagrange)
-	if err != nil {
-		return nil, nil, nil, err
-	}
-	return baseCcs, innerPK, innerVK, nil
-}
-
 func setupCircuitParams(innerField *big.Int) (constraint.ConstraintSystem, native_plonk.ProvingKey, native_plonk.VerifyingKey, error) {
 
 	innerCcs, err := frontend.Compile(innerField, scs.NewBuilder,
@@ -254,9 +210,15 @@ func TestSuccintRecurse(t *testing.T) {
 	//innerCcs, innerVK, innerWitness, innerProof := computeInnerProof(ecc.BLS12_377.ScalarField())
 	//computeInnerProofPlonk(ecc.BLS12_377.ScalarField())
 
+	//issuance txn
+	fullTxGenesisBytes, _ := hex.DecodeString("020000000190bc0a14e94cdd565265d79c4f9bed0f6404241f3fb69d6458b30b41611317f7000000004847304402204e643ff6ed0e3c3e1e83f3e2c74a9d0613849bb624c1d12351f1152cf91ebc1f02205deaa38e3f8f8e43d1979f999c03ffa65b9087c1a6545ecffa2b7898c042bcb241feffffff0200ca9a3b000000001976a914662db6c1a68cdf035bfb9c6580550eb3520caa9d88ac40276bee000000001976a9142dbbeab87bd7a8fca8b2761e5d798dfd76d5af4988ac6f000000")
+	prefixGenesisBytes, _ := hex.DecodeString("0200000001")
+	prevTxnIdGenesisBytes, _ := hex.DecodeString("90bc0a14e94cdd565265d79c4f9bed0f6404241f3fb69d6458b30b41611317f7")
+	postFixGenesisBytes, _ := hex.DecodeString("000000004847304402204e643ff6ed0e3c3e1e83f3e2c74a9d0613849bb624c1d12351f1152cf91ebc1f02205deaa38e3f8f8e43d1979f999c03ffa65b9087c1a6545ecffa2b7898c042bcb241feffffff0200ca9a3b000000001976a914662db6c1a68cdf035bfb9c6580550eb3520caa9d88ac40276bee000000001976a9142dbbeab87bd7a8fca8b2761e5d798dfd76d5af4988ac6f000000")
+
 	//innerCcs, innerVK, innerWitness, innerProof :=
 	assert := test.NewAssert(t)
-	innerCcs, innerVK, _, innerWitness, innerProof := createBaseCaseProof()
+	innerCcs, innerVK, _, innerWitness, innerProof := createBaseCaseProof(fullTxGenesisBytes, prefixGenesisBytes, prevTxnIdGenesisBytes, postFixGenesisBytes)
 
 	circuitVk, err := plonk.ValueOfVerifyingKey[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine](innerVK)
 	if err != nil {
@@ -276,30 +238,17 @@ func TestSuccintRecurse(t *testing.T) {
 		PreviousVk:      circuitVk,
 		PreviousWitness: plonk.PlaceholderWitness[sw_bls12377.ScalarField](innerCcs),
 	}
-	outerAssignment := &Sha256Circuit[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine, sw_bls12377.GT]{
-		PreviousWitness: circuitWitness,
-		PreviousProof:   circuitProof,
-	}
 
+	//spending transaction
 	prefixBytes, _ := hex.DecodeString("0200000001")
 	prevTxnIdBytes, _ := hex.DecodeString("faf3013aab53ae122e6cfdef7720c7a785fed4ce7f8f3dd19379f31e62651c71")
 	postFixBytes, _ := hex.DecodeString("000000006a47304402200ce76e906d995091f28ca40f4579c358bce832cd0d5c5535e4736e4444f6ba2602204fa80867c48e6016b3fa013633ad87203a18487786d8758ee3fe8a6ad5efdf06412103f368e789ce7c6152cc3a36f9c68e69b93934ce0b8596f9cd8032061d5feff4fffeffffff020065cd1d000000001976a914662db6c1a68cdf035bfb9c6580550eb3520caa9d88ac1e64cd1d000000001976a914ce3e1e6345551bed999b48ab8b2ebb1ca880bcda88ac70000000")
 
 	fullTxBytes, _ := hex.DecodeString("0200000001faf3013aab53ae122e6cfdef7720c7a785fed4ce7f8f3dd19379f31e62651c71000000006a47304402200ce76e906d995091f28ca40f4579c358bce832cd0d5c5535e4736e4444f6ba2602204fa80867c48e6016b3fa013633ad87203a18487786d8758ee3fe8a6ad5efdf06412103f368e789ce7c6152cc3a36f9c68e69b93934ce0b8596f9cd8032061d5feff4fffeffffff020065cd1d000000001976a914662db6c1a68cdf035bfb9c6580550eb3520caa9d88ac1e64cd1d000000001976a914ce3e1e6345551bed999b48ab8b2ebb1ca880bcda88ac70000000")
 
-	firstHash := sha256.Sum256(fullTxBytes)
-	currTxId := sha256.Sum256(firstHash[:])
+	outerAssignment := createOuterAssignment(circuitWitness, circuitProof, prefixBytes, prevTxnIdBytes, postFixBytes, fullTxBytes)
 
-	copy(outerAssignment.CurrTxPrefix[:], uints.NewU8Array(prefixBytes))
-	copy(outerAssignment.CurrTxPost[:], uints.NewU8Array(postFixBytes))
-	copy(outerAssignment.PrevTxId[:], uints.NewU8Array(prevTxnIdBytes))
-	copy(outerAssignment.CurrTxId[:], uints.NewU8Array(currTxId[:]))
-
-	tokenId := [32]byte{}
-	copy(tokenId[:], prevTxnIdBytes)
-	copy(outerAssignment.TokenId[:], uints.NewU8Array(tokenId[:]))
-
-	err = test.IsSolved(outerCircuit, outerAssignment, ecc.BW6_761.ScalarField())
+	err = test.IsSolved(outerCircuit, &outerAssignment, ecc.BW6_761.ScalarField())
 	assert.NoError(err)
 
 }
