3a89abf4bbfcf1f1d9bff19cb64b045d49afcdc8
Stephan February <stephan.february@token.im>
2024-03-08 09:10:34 +0800

    Refactored for BaseCase and NormalCase Setup

--- recurse/txv2recurse_test.go
+++ recurse/txv2recurse_test.go
@@ -35,7 +35,7 @@ func TestBaseCase(t *testing.T) {
 	//fmt.Println(hex.EncodeToString(genesisTxId[:]))
 	// create full genesis witness (placeholders, prevTxnIdBytes is empty
 	//vk, err := plonk.ValueOfVerifyingKey[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine](verifyingKey)
-	genesisWitness, err := createBaseCaseWitness(prefixBytes, postFixBytes, prevTxnIdBytes, genesisTxId)
+	genesisWitness, err := CreateBaseCaseWitness(prefixBytes, postFixBytes, prevTxnIdBytes, genesisTxId)
 
 	assert.NoError(err)
 	genesisProof, err := native_plonk.Prove(innerCcs, provingKey, genesisWitness, plonk.GetNativeProverOptions(outerField, innerField))
@@ -61,7 +61,8 @@ func TestNormalCase(t *testing.T) {
 	prevTxnIdBytes, _ := hex.DecodeString("90bc0a14e94cdd565265d79c4f9bed0f6404241f3fb69d6458b30b41611317f7")
 	postFixBytes, _ := hex.DecodeString("000000004847304402204e643ff6ed0e3c3e1e83f3e2c74a9d0613849bb624c1d12351f1152cf91ebc1f02205deaa38e3f8f8e43d1979f999c03ffa65b9087c1a6545ecffa2b7898c042bcb241feffffff0200ca9a3b000000001976a914662db6c1a68cdf035bfb9c6580550eb3520caa9d88ac40276bee000000001976a9142dbbeab87bd7a8fca8b2761e5d798dfd76d5af4988ac6f000000")
 
-	innerCcs, verifyingKey, _, genesisWitness, genesisProof := createBaseCaseProof(fullTxBytes, prefixBytes, prevTxnIdBytes, postFixBytes)
+	baseCcs, basePk, baseVk, err := SetupBaseCase(innerField)
+	genesisWitness, genesisProof, err := CreateBaseCaseProof(fullTxBytes, prefixBytes, prevTxnIdBytes, postFixBytes, baseCcs, basePk)
 
 	//can create a lightweight witness here for verification
 	//err := native_plonk.Verify(genesisProof, verifyingKey, genesisWitness, plonk.GetNativeVerifierOptions(outerField, innerField))
@@ -70,7 +71,7 @@ func TestNormalCase(t *testing.T) {
 	innerWitness, err := plonk.ValueOfWitness[sw_bls12377.ScalarField](genesisWitness)
 	innerProof, err := plonk.ValueOfProof[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine](genesisProof)
 
-	innerVk, err := plonk.ValueOfVerifyingKey[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine](verifyingKey)
+	innerVk, err := plonk.ValueOfVerifyingKey[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine](baseVk)
 
 	//spending tx info
 	prefixBytes, _ = hex.DecodeString("0200000001")
@@ -78,10 +79,10 @@ func TestNormalCase(t *testing.T) {
 	postFixBytes, _ = hex.DecodeString("000000006a47304402200ce76e906d995091f28ca40f4579c358bce832cd0d5c5535e4736e4444f6ba2602204fa80867c48e6016b3fa013633ad87203a18487786d8758ee3fe8a6ad5efdf06412103f368e789ce7c6152cc3a36f9c68e69b93934ce0b8596f9cd8032061d5feff4fffeffffff020065cd1d000000001976a914662db6c1a68cdf035bfb9c6580550eb3520caa9d88ac1e64cd1d000000001976a914ce3e1e6345551bed999b48ab8b2ebb1ca880bcda88ac70000000")
 	fullTxBytes, _ = hex.DecodeString("0200000001faf3013aab53ae122e6cfdef7720c7a785fed4ce7f8f3dd19379f31e62651c71000000006a47304402200ce76e906d995091f28ca40f4579c358bce832cd0d5c5535e4736e4444f6ba2602204fa80867c48e6016b3fa013633ad87203a18487786d8758ee3fe8a6ad5efdf06412103f368e789ce7c6152cc3a36f9c68e69b93934ce0b8596f9cd8032061d5feff4fffeffffff020065cd1d000000001976a914662db6c1a68cdf035bfb9c6580550eb3520caa9d88ac1e64cd1d000000001976a914ce3e1e6345551bed999b48ab8b2ebb1ca880bcda88ac70000000")
 
-	outerAssignment := createOuterAssignment(innerWitness, innerProof, innerVk, prefixBytes, prevTxnIdBytes, postFixBytes, fullTxBytes)
+	outerAssignment := CreateOuterAssignment(innerWitness, innerProof, innerVk, prefixBytes, prevTxnIdBytes, postFixBytes, fullTxBytes)
 	outerWitness, err := frontend.NewWitness(&outerAssignment, outerField)
 
-	outerCcs, outerProvingKey, outerVerifyingKey, err := SetupNormalCase(outerField, innerCcs, innerVk)
+	outerCcs, outerProvingKey, outerVerifyingKey, err := SetupNormalCase(outerField, baseCcs, innerVk)
 
 	assert.NoError(err)
 	outerProof, err := native_plonk.Prove(outerCcs, outerProvingKey, outerWitness, plonk.GetNativeProverOptions(outerField, innerField))
@@ -104,6 +105,10 @@ func TestNormalCaseSuccint(t *testing.T) {
 	//innerCcs, innerVK, innerWitness, innerProof := computeInnerProof(ecc.BLS12_377.ScalarField())
 	//computeInnerProofPlonk(ecc.BLS12_377.ScalarField())
 
+	innerField := ecc.BLS12_377.ScalarField()
+	//outerField := ecc.BLS12_377.ScalarField()
+	//outerField := ecc.BW6_761.ScalarField()
+
 	//issuance txn
 	fullTxGenesisBytes, _ := hex.DecodeString("020000000190bc0a14e94cdd565265d79c4f9bed0f6404241f3fb69d6458b30b41611317f7000000004847304402204e643ff6ed0e3c3e1e83f3e2c74a9d0613849bb624c1d12351f1152cf91ebc1f02205deaa38e3f8f8e43d1979f999c03ffa65b9087c1a6545ecffa2b7898c042bcb241feffffff0200ca9a3b000000001976a914662db6c1a68cdf035bfb9c6580550eb3520caa9d88ac40276bee000000001976a9142dbbeab87bd7a8fca8b2761e5d798dfd76d5af4988ac6f000000")
 	prefixGenesisBytes, _ := hex.DecodeString("0200000001")
@@ -112,9 +117,10 @@ func TestNormalCaseSuccint(t *testing.T) {
 
 	//innerCcs, innerVK, innerWitness, innerProof :=
 	assert := test.NewAssert(t)
-	innerCcs, innerVK, _, innerWitness, innerProof := createBaseCaseProof(fullTxGenesisBytes, prefixGenesisBytes, prevTxnIdGenesisBytes, postFixGenesisBytes)
+	baseCcs, basePk, baseVk, err := SetupBaseCase(innerField)
+	innerWitness, innerProof, err := CreateBaseCaseProof(fullTxGenesisBytes, prefixGenesisBytes, prevTxnIdGenesisBytes, postFixGenesisBytes, baseCcs, basePk)
 
-	circuitVk, err := plonk.ValueOfVerifyingKey[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine](innerVK)
+	circuitVk, err := plonk.ValueOfVerifyingKey[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine](baseVk)
 	if err != nil {
 		panic(err)
 	}
@@ -128,9 +134,9 @@ func TestNormalCaseSuccint(t *testing.T) {
 	}
 
 	outerCircuit := &Sha256Circuit[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine, sw_bls12377.GT]{
-		PreviousProof:   plonk.PlaceholderProof[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine](innerCcs),
+		PreviousProof:   plonk.PlaceholderProof[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine](baseCcs),
 		PreviousVk:      circuitVk,
-		PreviousWitness: plonk.PlaceholderWitness[sw_bls12377.ScalarField](innerCcs),
+		PreviousWitness: plonk.PlaceholderWitness[sw_bls12377.ScalarField](baseCcs),
 	}
 
 	//spending transaction
@@ -140,7 +146,7 @@ func TestNormalCaseSuccint(t *testing.T) {
 
 	fullTxBytes, _ := hex.DecodeString("0200000001faf3013aab53ae122e6cfdef7720c7a785fed4ce7f8f3dd19379f31e62651c71000000006a47304402200ce76e906d995091f28ca40f4579c358bce832cd0d5c5535e4736e4444f6ba2602204fa80867c48e6016b3fa013633ad87203a18487786d8758ee3fe8a6ad5efdf06412103f368e789ce7c6152cc3a36f9c68e69b93934ce0b8596f9cd8032061d5feff4fffeffffff020065cd1d000000001976a914662db6c1a68cdf035bfb9c6580550eb3520caa9d88ac1e64cd1d000000001976a914ce3e1e6345551bed999b48ab8b2ebb1ca880bcda88ac70000000")
 
-	outerAssignment := createOuterAssignment(circuitWitness, circuitProof, circuitVk, prefixBytes, prevTxnIdBytes, postFixBytes, fullTxBytes)
+	outerAssignment := CreateOuterAssignment(circuitWitness, circuitProof, circuitVk, prefixBytes, prevTxnIdBytes, postFixBytes, fullTxBytes)
 
 	err = test.IsSolved(outerCircuit, &outerAssignment, ecc.BW6_761.ScalarField())
 	assert.NoError(err)
